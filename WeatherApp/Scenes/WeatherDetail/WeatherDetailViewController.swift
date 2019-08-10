4d85bb93932abbf2e2a9bad74031ae9f07404d8b
yang hee jung <puuurm@naver.com>
2019-08-10 21:53:31 +0900

    Add: WeatherDetailViewController

---
+++ WeatherApp/Scenes/WeatherDetail/WeatherDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDetailViewController.swift
+//  WeatherApp
+//
+//  Created by yangpc on 10/08/2019.
+//  Copyright © 2019 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
