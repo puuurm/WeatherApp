7612eceb929a909de95b059152a0bf602e9f9bf1
yang hee jung <puuurm@naver.com>
2019-08-10 21:45:38 +0900

    Add: LocationSearchViewController

---
+++ WeatherApp/Scenes/LocationSearch/LocationSearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchViewController.swift
+//  WeatherApp
+//
+//  Created by yangpc on 10/08/2019.
+//  Copyright © 2019 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
