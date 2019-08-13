ee7d8c264c2561bf7a36cb8ed40844a17cca3065
yang hee jung <puuurm@naver.com>
2019-08-13 23:38:36 +0900

    Add: 네트워크 기능 구현한 ServerAccess와 test code 추가

---
+++ WeatherApp/ServerAccess.swift
@@ -0,0 +1,77 @@
+//
+//  ServerAccess.swift
+//  WeatherApp
+//
+//  Created by yangpc on 13/08/2019.
+//  Copyright © 2019 yang hee jung. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinate: Codable {
+    private(set) var latitude: Double
+    private(set) var longitude: Double
+}
+
+enum ServerAccess {
+
+    static let defaultSession = URLSession(configuration: .default)
+
+    static func request(coordinate: Coordinate,
+                        onSuccess: @escaping (String) -> Void,
+                        onFailure: @escaping (Error) -> Void) {
+
+        let urlString = BaseURL.forecast + DarkSky.apiKey + "/\(coordinate.latitude),\(coordinate.longitude)"
+
+        guard let url = URL(string: urlString) else { return }
+
+        defaultSession.dataTask(with: url) { (data, response, error) in
+
+            guard error == nil else {
+                onFailure(error!)
+                return
+            }
+
+            guard let data = data, !data.isEmpty else {
+                onFailure(MyError.responseError)
+                return
+            }
+
+            do {
+
+                let jsonObject = try JSONSerialization.jsonObject(with: data) as! [String : Any]
+                let jsonData = try JSONSerialization.data(withJSONObject: jsonObject)
+                let jsonString = String(data: jsonData, encoding: .utf8) ?? ""
+                let data = jsonString.data(using: .utf8)!
+
+                if let error = try? JSONDecoder().decode(ServerError.self, from: data) {
+                    onFailure(MyError.serverError(code: error.code, error: error.error))
+                    return
+                }
+                onSuccess(jsonString)
+            } catch {
+                onFailure(error)
+            }
+        }.resume()
+    }
+
+}
+
+enum MyError: Error {
+    case responseError
+    case serverError(code: Int, error: String)
+}
+
+struct ServerError: Codable {
+    var code: Int
+    var error: String
+}
+
+
+enum BaseURL {
+    static let forecast = "https://api.darksky.net/forecast/"
+}
+
+enum DarkSky {
+    static let apiKey = "8c97b162f1c44e6c82c6505e6a6ec19e"
+}
---
+++ WeatherAppTests/ServerAccessTest.swift
@@ -0,0 +1,41 @@
+//
+//  ServerAccessTest.swift
+//  WeatherAppTests
+//
+//  Created by yangpc on 13/08/2019.
+//  Copyright © 2019 yang hee jung. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherApp
+
+class ServerAccessTest: XCTestCase {
+
+    func testServerRequestSuccess() {
+        let coordinate = Coordinate(latitude: 37.5868661, longitude: 126.7578747)
+        let expectation = self.expectation(description: #function)
+
+        ServerAccess
+            .request(coordinate: coordinate, onSuccess: { (jsonString) in
+                XCTAssertNotNil(jsonString)
+                expectation.fulfill()
+            }) { (error) in }
+
+        waitForExpectations(timeout: 3)
+    }
+
+    func testServerRequestClientError() {
+        let coordinate = Coordinate(latitude: 11111111, longitude: 11111)
+        let expectation = self.expectation(description: #function)
+        ServerAccess
+            .request(coordinate: coordinate, onSuccess: { (jsonString) in
+            }) { (error) in
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 3)
+    }
+
+
+}
