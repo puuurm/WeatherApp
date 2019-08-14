d24d82d112f085bb43f157b2015b87be40becce5
yang hee jung <puuurm@naver.com>
2019-08-15 04:56:00 +0900

    Update: 날씨 관련 모델 정의

--- WeatherApp/Model.swift
+++ WeatherApp/Model.swift
@@ -13,3 +13,131 @@ struct Coordinate: Codable {
     private(set) var longitude: Double
 }
 
+struct Response: Codable {
+    var timezone: String
+    var currently: CurrentlyWeatherData
+    var hourly: HourlyWeatherInfo
+    var daily: DailyWeatherInfo
+}
+
+
+struct DailyWeatherInfo: Codable {
+    var data: [DailyWeatherData]
+    var summary: String
+    var icon: String
+}
+
+struct HourlyWeatherInfo: Codable {
+    var data: [HourlyWeatherData]
+    var summary: String?
+    var icon: String?
+}
+
+struct CurrentlyWeatherData: CommonWeatherData {
+    var cloudCover: Float?
+    var dewPoint: Double?
+    var humidity: Float?
+    var icon: String?
+    var ozone: Float?
+    var precipIntensity: Double?
+    var precipIntensityError: Double?
+    var precipProbability: Double?
+    var precipType: String?
+    var pressure: Double?
+    var summary: String?
+    var time: Int!
+    var uvIndex: Float?
+    var visibility: Float?
+    var windBearing: Double?
+    var windGust: Float?
+    var windSpeed: Float?
+    var nearestStormDistance: Double?
+    var nearestStormBearing: Double?
+}
+
+struct HourlyWeatherData: CommonWeatherData {
+    var cloudCover: Float?
+    var dewPoint: Double?
+    var humidity: Float?
+    var icon: String?
+    var ozone: Float?
+    var precipIntensity: Double?
+    var precipIntensityError: Double?
+    var precipProbability: Double?
+    var precipType: String?
+    var pressure: Double?
+    var summary: String?
+    var time: Int!
+    var uvIndex: Float?
+    var visibility: Float?
+    var windBearing: Double?
+    var windGust: Float?
+    var windSpeed: Float?
+    var apparentTemperature: Double?
+    var precipAccumulation: Double?
+    var temperature: Double?
+}
+
+struct DailyWeatherData: CommonWeatherData {
+    var cloudCover: Float?
+    var dewPoint: Double?
+    var humidity: Float?
+    var icon: String?
+    var ozone: Float?
+    var precipIntensity: Double?
+    var precipIntensityError: Double?
+    var precipProbability: Double?
+    var precipType: String?
+    var pressure: Double?
+    var summary: String?
+    var time: Int!
+    var uvIndex: Float?
+    var visibility: Float?
+    var windBearing: Double?
+    var windGust: Float?
+    var windSpeed: Float?
+    var apparentTemperatureHigh: Double?
+    var apparentTemperatureHighTime: Double?
+    var apparentTemperatureLow: Double?
+    var apparentTemperatureLowTime: Double?
+    var apparentTemperatureMax: Double?
+    var apparentTemperatureMaxTime: Double?
+    var apparentTemperatureMin: Double?
+    var apparentTemperatureMinTime: Double?
+    var moonPhase: Double?
+    var precipAccumulation: Double?
+    var precipIntensityMax: Double?
+    var precipIntensityMaxTime: Double?
+    var sunriseTime: Double?
+    var sunsetTime: Double?
+    var temperatureHigh: Double?
+    var temperatureHighTime: Double?
+    var temperatureLow: Double?
+    var temperatureLowTime: Double?
+    var temperatureMax: Double?
+    var temperatureMaxTime: Double?
+    var temperatureMin: Double?
+    var temperatureMinTime: Double?
+    var uvIndexTime: Double?
+    var windGustTime: Double?
+}
+
+protocol CommonWeatherData: Codable {
+    var cloudCover: Float? { get set }
+    var dewPoint: Double? { get set }
+    var humidity: Float? { get set }
+    var icon: String? { get set }
+    var ozone: Float? { get set }
+    var precipIntensity: Double? { get set }
+    var precipIntensityError: Double? { get set }
+    var precipProbability: Double? { get set }
+    var precipType: String? { get set }
+    var pressure: Double? { get set }
+    var summary: String? { get set }
+    var time: Int! { get set }
+    var uvIndex: Float? { get set }
+    var visibility: Float? { get set }
+    var windBearing: Double? { get set }
+    var windGust: Float? { get set }
+    var windSpeed: Float? { get set }
+}
