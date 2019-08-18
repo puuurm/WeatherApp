6cdf471bb542c9ba7360b2573ce165fddd7967b3
yang hee jung <puuurm@naver.com>
2019-08-18 22:38:48 +0900

    Add: TodayWeatherDetail관련 뷰

---
+++ WeatherApp/Scenes/WeatherDetail/Views/TodayWeatherDetailCell.swift
@@ -0,0 +1,21 @@
+//
+//  TodayWeatherDetailCell.swift
+//  WeatherApp
+//
+//  Created by yangpc on 18/08/2019.
+//  Copyright © 2019 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class TodayWeatherDetailCell: UICollectionViewCell, TypeIdentifiable {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ WeatherApp/Scenes/WeatherDetail/Views/TodayWeatherDetailCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  TodayWeatherDetailCollectionViewCell.swift
+//  WeatherApp
+//
+//  Created by yangpc on 18/08/2019.
+//  Copyright © 2019 yang hee jung. All rights reserved.
+//
+
+import UIKit
+
+class TodayWeatherDetailCollectionViewCell: UICollectionViewCell, TypeIdentifiable {
+
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.isScrollEnabled = false
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+
+    func setupViews() {
+        contentView.addSubview(collectionView)
+        collectionView.fillSuperview()
+        collectionView.register(
+            UINib(nibName: DailyWeatherCell.identifier, bundle: nil),
+            forCellWithReuseIdentifier: DailyWeatherCell.identifier
+        )
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
